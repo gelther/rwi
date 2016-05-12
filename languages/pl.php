f2052cd67e7b22e97cf410a4fe2dd77d50abe741
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:01:30 +0200

    Code consistency changes for languages/pl.php

--- languages/pl.php
+++ languages/pl.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Okropny",
-        "ratePoor" => "Słaby",
-        "rateAverage" => "Przeciętny",
-        "rateGood" => "Dobry",
-        "rateExcellent" => "Wspaniały",
-        "rateThis" => "Oceń to",
-        "like" => "Lubię",
-        "dislike" => "Nie lubię",
-        "vote" => "Głosuj",
-        "votes" => "Głosy",
-        "thanks" => "Dziękuję",
-        "outOf" => "z",
-        "weRecommend" => "Zalecamy",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Okropny',
+		'ratePoor'      => 'Słaby',
+		'rateAverage'   => 'Przeciętny',
+		'rateGood'      => 'Dobry',
+		'rateExcellent' => 'Wspaniały',
+		'rateThis'      => 'Oceń to',
+		'like'          => 'Lubię',
+		'dislike'       => 'Nie lubię',
+		'vote'          => 'Głosuj',
+		'votes'         => 'Głosy',
+		'thanks'        => 'Dziękuję',
+		'outOf'         => 'z',
+		'weRecommend'   => 'Zalecamy',
+	);
