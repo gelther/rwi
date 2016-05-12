7835a9ad2df3839cec3d034befcda4f230bf788f
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:45:43 +0200

    Code consistency changes for view/site/multi-rating.php

--- view/site/multi-rating.php
+++ view/site/multi-rating.php
@@ -21,12 +21,12 @@ $rw_no_labels = true;
 foreach ( $mr_multi_options->criteria as $criteria_key => $criteria ) {
     $rclass = $mr_element_class;
     $add_schema = $mr_add_schema;
-    
+
     if ( $multi_criteria ) {
         $rclass .= '-criteria-' . $criteria_id;
         // If summary rating is visible, don't add snippets because they are
         // already added through the 2nd call to the EmbedRawRating method below.
-        
+
         if ( $mr_multi_options->show_summary_rating ) {
             $add_schema = false;
         } else {
@@ -37,9 +37,9 @@ foreach ( $mr_multi_options->criteria as $criteria_key => $criteria ) {
                 $add_schema = false;
             }
         }
-    
+
     }
-    
+
     $criteria_urid = ratingwidget()->get_rating_id_by_element( $mr_element_id, $mr_element_class, ( $multi_criteria ? $criteria_id++ : false ) );
     $raw_rating = ratingwidget()->EmbedRawRating(
         $criteria_urid,
@@ -52,7 +52,7 @@ foreach ( $mr_multi_options->criteria as $criteria_key => $criteria ) {
         $mr_embed_options
     );
     // Defaults to &nbsp; instead of empty to keep the widths of all rating widgets same
-    
+
     if ( isset( $criteria['label'] ) ) {
         $label = $criteria['label'];
         if ( $rw_no_labels ) {
@@ -61,7 +61,7 @@ foreach ( $mr_multi_options->criteria as $criteria_key => $criteria ) {
     } else {
         $label = '&nbsp;';
     }
-    
+
     $html .= '<tr>';
     $html .= '<td><nobr>' . $label . '</nobr></td>';
     $html .= '<td>' . $raw_rating . '</td>';
@@ -69,24 +69,24 @@ foreach ( $mr_multi_options->criteria as $criteria_key => $criteria ) {
 }
 
 if ( !empty($html) ) {
-    
+
     if ( $mr_multi_options->show_summary_rating && $multi_criteria ) {
-        
+
         if ( $hide_recommendations ) {
             // Restore the value of hide-recommendations
             $mr_embed_options['hide-recommendations'] = 'true';
         } else {
             unset( $mr_embed_options['hide-recommendations'] );
         }
-        
-        
+
+
         if ( false === $original_uarid ) {
             unset( $mr_embed_options['uarid'] );
         } else {
             // Restore original aggregated rating ID.
             $mr_embed_options['uarid'] = $original_uarid;
         }
-        
+
         $mr_embed_options['read-only'] = 'true';
         $mr_embed_options['force-sync'] = 'true';
         $raw_rating = ratingwidget()->EmbedRawRating(
@@ -100,7 +100,7 @@ if ( !empty($html) ) {
             $mr_embed_options
         );
         // Defaults to &nbsp; instead of empty to keep the widths of all rating widgets same
-        
+
         if ( isset( $mr_multi_options->summary_label ) ) {
             $summary_label = $mr_multi_options->summary_label;
             if ( $rw_no_labels ) {
@@ -109,13 +109,13 @@ if ( !empty($html) ) {
         } else {
             $summary_label = '&nbsp;';
         }
-        
+
         $html .= '<tr>';
         $html .= '<td><nobr>' . $summary_label . '</nobr></td>';
         $html .= '<td>' . $raw_rating . '</td>';
         $html .= '</tr>';
     }
-    
+
     $dir = ( isset( $mr_general_options->advanced ) && isset( $mr_general_options->advanced->layout ) && is_string( $mr_general_options->advanced->layout->dir ) ? $mr_general_options->advanced->layout->dir : 'ltr' );
     $table_classes = 'rw-rating-table rw-' . $dir;
     $table_classes .= ( false !== $mr_hor_align ? ' rw-' . $mr_hor_align : '' );
