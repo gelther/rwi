fee923ea10c7a170d5057d733db0bfe181074ff3
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:35:37 +0200

    Code consistency changes for view/settings/size.php

--- view/settings/size.php
+++ view/settings/size.php
@@ -3,13 +3,13 @@
 $selected_size = rw_options()->size;
 ?>
 <td class="rw-ui-def-width">
-	<span class="rw-ui-def"><?php 
+	<span class="rw-ui-def"><?php
 _erw( 'size' );
 ?>
 :</span>
 </td>
 <td>
-	<?php 
+	<?php
 $sizes = array(
     'tiny'   => __rw( 'tiny' ),
     'small'  => __rw( 'small' ),
@@ -19,36 +19,36 @@ $sizes = array(
 $tab_index = 5;
 foreach ( $sizes as $size => $label ) {
     ?>
-			<div class="rw-ui-img-radio<?php 
+			<div class="rw-ui-img-radio<?php
     if ( $selected_size == $size ) {
         echo  ' rw-selected' ;
     }
     ?>
-" onclick="RWM.Set.size(RW.SIZE.<?php 
+" onclick="RWM.Set.size(RW.SIZE.<?php
     echo  strtoupper( $size ) ;
     ?>
 );">
 				<i class="rw-ui-holder"><i
-						class="rw-ui-sprite rw-ui-star rw-ui-<?php 
+						class="rw-ui-sprite rw-ui-star rw-ui-<?php
     echo  $size ;
     ?>
  rw-ui-yellow"></i></i>
-				<span><?php 
+				<span><?php
     echo  $label ;
     ?>
 </span>
-				<input type="radio" tabindex="<?php 
+				<input type="radio" tabindex="<?php
     echo  $tab_index ;
     ?>
 " name="rw-size"
-				       value="0"<?php 
+				       value="0"<?php
     if ( $selected_size == $size ) {
         echo  ' checked="checked"' ;
     }
     ?>
  />
 			</div>
-			<?php 
+			<?php
     $tab_index++;
 }
 ?>
