d0ac70b46378fdf8bc30a65c59c867930acf04f4
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:01:48 +0200

    Code consistency changes for languages/tr.php

--- languages/tr.php
+++ languages/tr.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Çok Kötü",
-        "ratePoor" => "Kötü",
-        "rateAverage" => "Orta",
-        "rateGood" => "İyi",
-        "rateExcellent" => "Mükemmel",
-        "rateThis" => "Değerlendir",
-        "like" => "Beğen",
-        "dislike" => "Beğenme",
-        "vote" => "Oyla",
-        "votes" => "Oylar",
-        "thanks" => "Teşekkürler",
-        "outOf" => "üzerinden",
-        "weRecommend" => "Biz Tavsiye",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Çok Kötü',
+		'ratePoor'      => 'Kötü',
+		'rateAverage'   => 'Orta',
+		'rateGood'      => 'İyi',
+		'rateExcellent' => 'Mükemmel',
+		'rateThis'      => 'Değerlendir',
+		'like'          => 'Beğen',
+		'dislike'       => 'Beğenme',
+		'vote'          => 'Oyla',
+		'votes'         => 'Oylar',
+		'thanks'        => 'Teşekkürler',
+		'outOf'         => 'üzerinden',
+		'weRecommend'   => 'Biz Tavsiye',
+	);
