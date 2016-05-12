7735d3015c33ecee531e82b7345468bbe3704e3c
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:45:23 +0200

    Code consistency changes for view/user_rating_type_options.php

--- view/user_rating_type_options.php
+++ view/user_rating_type_options.php
@@ -6,49 +6,49 @@ $is_accumulated = ( $bbpress_support ? rw_settings()->is_user_accumulated : fals
 <div id="rw_user_rating_type_settings" class="has-sidebar has-right-sidebar">
 	<div class="has-sidebar-content">
 		<div class="postbox rw-body">
-			<h3><?php 
+			<h3><?php
 _erw( 'rating-type' );
 ?>
 </h3>
 
 			<div class="inside rw-ui-content-container rw-no-radius">
-				<div class="rw-ui-img-radio rw-ui-hor<?php 
+				<div class="rw-ui-img-radio rw-ui-hor<?php
 if ( $is_accumulated ) {
     echo  ' rw-selected' ;
 }
 ?>
-"<?php 
+"<?php
 if ( !$bbpress_support ) {
     echo  ' data-alert="' . __rw( 'reputational-rating_pro-only-alert' ) . '"' ;
 }
 ?>
 >
 					<i class="rw-ui-sprite"></i> <input type="radio" name="rw_accumulated_user_rating"
-					                                    value="true" <?php 
+					                                    value="true" <?php
 if ( $is_accumulated ) {
     echo  ' checked="checked"' ;
 }
 ?>
 >
-					<span><?php 
+					<span><?php
 _erw( 'reputational-rating_desc' );
 ?>
 </span>
 				</div>
-				<div class="rw-ui-img-radio rw-ui-hor<?php 
+				<div class="rw-ui-img-radio rw-ui-hor<?php
 if ( !$is_accumulated ) {
     echo  ' rw-selected' ;
 }
 ?>
 ">
 					<i class="rw-ui-sprite"></i> <input type="radio" name="rw_accumulated_user_rating"
-					                                    value="false" <?php 
+					                                    value="false" <?php
 if ( !$is_accumulated ) {
     echo  ' checked="checked"' ;
 }
 ?>
 >
-					<span><?php 
+					<span><?php
 _erw( 'standard-rating_desc' );
 ?>
 </span>
