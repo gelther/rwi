283ed5352e5552629717ec931ec3433ef4aa7d91
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:42:11 +0200

    Code consistency changes for themes/nero/n2.php

--- themes/nero/n2.php
+++ themes/nero/n2.php
@@ -6,10 +6,9 @@
     $theme_options->advanced->font = new stdClass();
     $theme_options->advanced->font->color = "#000000";
     $theme_options->advanced->font->hover = new stdClass();
-    $theme_options->advanced->font->hover->color = "#000";    
+    $theme_options->advanced->font->hover->color = "#000";
     $theme = array(
         "name" => "thumbs_2",
         "title" => "Thumbs 2 (Jochem)",
         "options" => $theme_options
     );
-?>
