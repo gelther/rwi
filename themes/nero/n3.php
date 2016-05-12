ce502dbb8e1f35b38bbac2f8ee0d8076e37bd053
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:41:45 +0200

    Code consistency changes for themes/nero/n3.php

--- themes/nero/n3.php
+++ themes/nero/n3.php
@@ -7,10 +7,9 @@
     $theme_options->advanced->font->color = "#000000";
     $theme_options->advanced->font->hover = new stdClass();
     $theme_options->advanced->font->hover->color = "#000";
-        
+
     $theme = array(
         "name" => "arrows_1",
         "title" => "Arrows 1",
         "options" => $theme_options
     );
-?>
