febf5abcaa10caa3a925a419d8174fbc9f945911
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:45:47 +0200

    Code consistency changes for view/star_rating.php

--- view/star_rating.php
+++ view/star_rating.php
@@ -17,21 +17,21 @@ if ( !function_exists( 'show_star_rating' ) ) {
         $font_size = 'font-size: ' . (( isset( $roptions->advanced->font->size ) ? $roptions->advanced->font->size : '12px' )) . ';';
         $line_height = 'line-height: ' . (( isset( $roptions->advanced->layout->lineHeight ) ? $roptions->advanced->layout->lineHeight : '16px' )) . ';';
         ?>
-<div class="rw-ui-container rw-no-render<?php 
+<div class="rw-ui-container rw-no-render<?php
         echo  $type . $size . $dir . $halign . $valign . $style ;
         ?>
 ">
-    <div class="rw-action-area rw-clearfix"><ul class="rw-ui-stars"><?php 
+    <div class="rw-action-area rw-clearfix"><ul class="rw-ui-stars"><?php
         $floor = floor( $rate );
         for ( $i = 0 ;  $i < $floor ;  $i++ ) {
             echo  '<li class="rw-ui-star-selected"></li>' ;
         }
         $diff = $rate - $floor;
-        
+
         if ( $diff > 0.75 ) {
             echo  '<li class="rw-ui-star-selected"></li>' ;
         } else {
-            
+
             if ( $diff > 0.25 && $diff < 0.75 ) {
                 echo  '<li class="rw-ui-star-half-selected"></li>' ;
             } else {
@@ -39,22 +39,22 @@ if ( !function_exists( 'show_star_rating' ) ) {
                     echo  '<li></li>' ;
                 }
             }
-        
+
         }
-        
+
         for ( $i = $i + 1 ;  $i < 5 ;  $i++ ) {
             echo  '<li></li>' ;
         }
         ?>
-</ul><a class="rw-report-link" target="_blank" title="Rating-Widget Report"></a></div><span class="rw-ui-info" style="<?php 
+</ul><a class="rw-report-link" target="_blank" title="Rating-Widget Report"></a></div><span class="rw-ui-info" style="<?php
         echo  $font_type . $font_color . $font_size . $line_height ;
         ?>
-"><?php 
+"><?php
         echo  $label ;
         ?>
 </span>
 </div>
-<?php 
+<?php
     }
 
 }
