da4590fff0cb04bb42b77a70042cfb9e84b9bf31
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:19:50 +0200

    Code consistency changes for languages/ro.php

--- languages/ro.php
+++ languages/ro.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Groaznic",
-        "ratePoor" => "rau",
-        "rateAverage" => "Mediu",
-        "rateGood" => "Bun",
-        "rateExcellent" => "Excelent",
-        "rateThis" => "Apreciaza",
-        "like" => "Imi place",
-        "dislike" => "Nu-mi place",
-        "vote" => "Voteaza",
-        "votes" => "Voturi",
-        "thanks" => "Multumim",
-        "outOf" => "din",
-        "weRecommend" => "Vă recomandăm",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Groaznic',
+		'ratePoor'      => 'rau',
+		'rateAverage'   => 'Mediu',
+		'rateGood'      => 'Bun',
+		'rateExcellent' => 'Excelent',
+		'rateThis'      => 'Apreciaza',
+		'like'          => 'Imi place',
+		'dislike'       => 'Nu-mi place',
+		'vote'          => 'Voteaza',
+		'votes'         => 'Voturi',
+		'thanks'        => 'Multumim',
+		'outOf'         => 'din',
+		'weRecommend'   => 'Vă recomandăm',
+	);
