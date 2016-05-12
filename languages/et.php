188c84a86bc912e5c069572f4b63310a2039ca9e
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:00:53 +0200

    Code consistency changes for languages/et.php

--- languages/et.php
+++ languages/et.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Kohutav",
-        "ratePoor" => "Kesine",
-        "rateAverage" => "Keskmine",
-        "rateGood" => "Hea",
-        "rateExcellent" => "Suurepärane",
-        "rateThis" => "Hinda seda",
-        "like" => "Meeldib",
-        "dislike" => "Ei meeldi",
-        "vote" => "Hinda",
-        "votes" => "Hääli",
-        "thanks" => "Täname",
-        "outOf" => "out of",
-        "weRecommend" => "Soovitame",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Kohutav',
+		'ratePoor'      => 'Kesine',
+		'rateAverage'   => 'Keskmine',
+		'rateGood'      => 'Hea',
+		'rateExcellent' => 'Suurepärane',
+		'rateThis'      => 'Hinda seda',
+		'like'          => 'Meeldib',
+		'dislike'       => 'Ei meeldi',
+		'vote'          => 'Hinda',
+		'votes'         => 'Hääli',
+		'thanks'        => 'Täname',
+		'outOf'         => 'out of',
+		'weRecommend'   => 'Soovitame',
+	);
