70876f5f771f5a2692e84ffd8ebda3f7094a99dd
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 13:33:02 +0200

    Code consistency changes for lib/rw-extension-abstract.php

--- lib/rw-extension-abstract.php
+++ lib/rw-extension-abstract.php
@@ -10,67 +10,67 @@ if ( class_exists( 'RatingWidgetPlugin' ) && !class_exists( 'RW_AbstractExtensio
          * @return string
          */
         public abstract function GetSlug();
-        
+
         /**
          * @return boolean
          */
         public abstract function HasSettingsMenu();
-        
+
         /**
          * @return array
          */
         public abstract function GetSettingsMenuItem();
-        
+
         /**
          * @return array
          */
         public abstract function GetSettings();
-        
+
         /**
          * @return array
          */
         public abstract function GetRatingClasses();
-        
+
         /**
          * @return array
          */
         public abstract function GetDefaultOptions();
-        
+
         /**
          * @return array
          */
         public abstract function GetDefaultAlign();
-        
+
         /**
          * @param $class string
          *
          * @return string
          */
         public abstract function GetAlignOptionNameByClass( $class );
-        
+
         /**
          * If true, page/post/comment ratings would be disabled on current page.
          *
          * @return boolean
          */
         public abstract function BlockLoopRatings();
-        
+
         /**
          * Check if the extension supports ratings for current page.
          *
          * @return boolean
          */
         public abstract function IsExtensionPage();
-        
+
         /**
          * Return the rating class of the current's page.
          *
          * @return string
          */
         public abstract function GetCurrentPageClass();
-        
+
         public abstract function Hook( $rclass );
-        
+
         /**
          * Retrieve unique global rating ID for the specific element.
          *
@@ -80,12 +80,12 @@ if ( class_exists( 'RatingWidgetPlugin' ) && !class_exists( 'RW_AbstractExtensio
          * @return mixed
          */
         public abstract function GetRatingGuid( $element_id, $rclass );
-        
+
         /**
          * @return array
          */
         public abstract function GetTopRatedInfo();
-        
+
         /**
          * @param $type string
          * @param $rating object
@@ -93,6 +93,6 @@ if ( class_exists( 'RatingWidgetPlugin' ) && !class_exists( 'RW_AbstractExtensio
          * @return array of string
          */
         public abstract function GetElementInfoByRating( $type, $rating );
-    
+
     }
 }
