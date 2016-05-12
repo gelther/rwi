1be0a558bf39e154a55c0627941e2e0cb3e4d2fd
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:19:54 +0200

    Code consistency changes for languages/sk.php

--- languages/sk.php
+++ languages/sk.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Strašný",
-        "ratePoor" => "Slabý",
-        "rateAverage" => "Priemerný",
-        "rateGood" => "Dobrý",
-        "rateExcellent" => "Výborný",
-        "rateThis" => "Hodnotenie",
-        "like" => "Páči sa mi to",
-        "dislike" => "Nepáči sa mi to",
-        "vote" => "Hlas",
-        "votes" => "Hlasy",
-        "thanks" => "Ďakujem",
-        "outOf" => "z",
-        "weRecommend" => "Odporúčame",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Strašný',
+		'ratePoor'      => 'Slabý',
+		'rateAverage'   => 'Priemerný',
+		'rateGood'      => 'Dobrý',
+		'rateExcellent' => 'Výborný',
+		'rateThis'      => 'Hodnotenie',
+		'like'          => 'Páči sa mi to',
+		'dislike'       => 'Nepáči sa mi to',
+		'vote'          => 'Hlas',
+		'votes'         => 'Hlasy',
+		'thanks'        => 'Ďakujem',
+		'outOf'         => 'z',
+		'weRecommend'   => 'Odporúčame',
+	);
