ea3bbc71c2eaca6e5ba7e416b9605602a7d1ac38
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:01:36 +0200

    Code consistency changes for languages/ru.php

--- languages/ru.php
+++ languages/ru.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Ужасный",
-        "ratePoor" => "Плохо",
-        "rateAverage" => "Среднее",
-        "rateGood" => "Хорошее",
-        "rateExcellent" => "Отлично",
-        "rateThis" => "Голосуй",
-        "like" => "Нравится",
-        "dislike" => "Не нравится",
-        "vote" => "Голос",
-        "votes" => "Голосов",
-        "thanks" => "Спасибо",
-        "outOf" => "из",
-        "weRecommend" => "Мы рекомендуем",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Ужасный',
+		'ratePoor'      => 'Плохо',
+		'rateAverage'   => 'Среднее',
+		'rateGood'      => 'Хорошее',
+		'rateExcellent' => 'Отлично',
+		'rateThis'      => 'Голосуй',
+		'like'          => 'Нравится',
+		'dislike'       => 'Не нравится',
+		'vote'          => 'Голос',
+		'votes'         => 'Голосов',
+		'thanks'        => 'Спасибо',
+		'outOf'         => 'из',
+		'weRecommend'   => 'Мы рекомендуем',
+	);
