a5a977ac915b4f7d34d932c5d42b918d9442e730
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-12 15:01:07 +0200

    Code consistency changes for languages/hu.php

--- languages/hu.php
+++ languages/hu.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
-    $dictionary = array(
-        "rateAwful" => "Elégtelen",
-        "ratePoor" => "Gyenge",
-        "rateAverage" => "Átlagos",
-        "rateGood" => "Jó",
-        "rateExcellent" => "Kitűnő",
-        "rateThis" => "Értékelje",
-        "like" => "Tetszik",
-        "dislike" => "Nem tetszik",
-        "vote" => "Szavazzon",
-        "votes" => "Szavazatok",
-        "thanks" => "Köszönöm",
-        "outOf" => "out of",
-        "weRecommend" => "Javasoljuk",
-    );
-?>
+	$dir        = 'ltr';
+	$hor        = 'right';
+	$dictionary = array(
+		'rateAwful'     => 'Elégtelen',
+		'ratePoor'      => 'Gyenge',
+		'rateAverage'   => 'Átlagos',
+		'rateGood'      => 'Jó',
+		'rateExcellent' => 'Kitűnő',
+		'rateThis'      => 'Értékelje',
+		'like'          => 'Tetszik',
+		'dislike'       => 'Nem tetszik',
+		'vote'          => 'Szavazzon',
+		'votes'         => 'Szavazatok',
+		'thanks'        => 'Köszönöm',
+		'outOf'         => 'out of',
+		'weRecommend'   => 'Javasoljuk',
+	);
