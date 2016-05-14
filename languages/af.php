b9a97f276043ec080a5a814f924a8496fad3cf7b
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 13:22:20 +0200

    Code consistency changes for languages/af.php

--- languages/af.php
+++ languages/af.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
+    $dir        = 'ltr';
+    $hor        = 'right';
     $dictionary = array(
-        "rateAwful" => "Verskriklik",
-        "ratePoor" => "Bad",
-        "rateAverage" => "Gemiddelde",
-        "rateGood" => "Goeie",
-        "rateExcellent" => "Uitstekend",
-        "rateThis" => "Beoordeel hierdie",
-        "like" => "Soos",
-        "dislike" => "Hou nie",
-        "vote" => "Stem",
-        "votes" => "Stemme",
-        "thanks" => "Dankie dat jy",
-        "outOf" => "uit",
-        "weRecommend" => "ons beveel aan",
+        'rateAwful'     => 'Verskriklik',
+        'ratePoor'      => 'Bad',
+        'rateAverage'   => 'Gemiddelde',
+        'rateGood'      => 'Goeie',
+        'rateExcellent' => 'Uitstekend',
+        'rateThis'      => 'Beoordeel hierdie',
+        'like'          => 'Soos',
+        'dislike'       => 'Hou nie',
+        'vote'          => 'Stem',
+        'votes'         => 'Stemme',
+        'thanks'        => 'Dankie dat jy',
+        'outOf'         => 'uit',
+        'weRecommend'   => 'ons beveel aan',
     );
-?>
