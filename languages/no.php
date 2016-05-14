4bcecb09e52258ba23083e7739ac466adeb14086
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 13:23:20 +0200

    Code consistency changes for languages/no.php

--- languages/no.php
+++ languages/no.php
@@ -1,18 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
+    $dir        = 'ltr';
+    $hor        = 'right';
     $dictionary = array(
-        "rateAwful" => "Elendig",
-        "ratePoor" => "Dårlig",
-        "rateAverage" => "Gjennomsnittlig",
-        "rateGood" => "Bra",
-        "rateExcellent" => "Utmerket",
-        "rateThis" => "Hva synes du om dette",
-        "like" => "Liker",
-        "dislike" => "Liker ikke",
-        "vote" => "Stemme",
-        "votes" => "Stemmer",
-        "thanks" => "Takk",
-        "outOf" => "av",
-        "weRecommend" => "Vi anbefaler",
+        'rateAwful'     => 'Elendig',
+        'ratePoor'      => 'Dårlig',
+        'rateAverage'   => 'Gjennomsnittlig',
+        'rateGood'      => 'Bra',
+        'rateExcellent' => 'Utmerket',
+        'rateThis'      => 'Hva synes du om dette',
+        'like'          => 'Liker',
+        'dislike'       => 'Liker ikke',
+        'vote'          => 'Stemme',
+        'votes'         => 'Stemmer',
+        'thanks'        => 'Takk',
+        'outOf'         => 'av',
+        'weRecommend'   => 'Vi anbefaler',
     );
