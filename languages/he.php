2476e24d0ee160322ddcd7b956910249e2f0e8a8
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 13:22:56 +0200

    Code consistency changes for languages/he.php

--- languages/he.php
+++ languages/he.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "rtl";
-    $hor = "left";
+    $dir        = 'rtl';
+    $hor        = 'left';
     $dictionary = array(
-        "rateAwful" => "גרוע",
-        "ratePoor" => "רע",
-        "rateAverage" => "ממוצע",
-        "rateGood" => "טוב",
-        "rateExcellent" => "מצויין",
-        "rateThis" => "דרג",
-        "like" => "בעד",
-        "dislike" => "נגד",
-        "vote" => "הצבעה",
-        "votes" => "הצבעות",
-        "thanks" => "תודה",
-        "outOf" => "מתוך",
-        "weRecommend" => "אנו ממליצים",
+        'rateAwful'     => 'גרוע',
+        'ratePoor'      => 'רע',
+        'rateAverage'   => 'ממוצע',
+        'rateGood'      => 'טוב',
+        'rateExcellent' => 'מצויין',
+        'rateThis'      => 'דרג',
+        'like'          => 'בעד',
+        'dislike'       => 'נגד',
+        'vote'          => 'הצבעה',
+        'votes'         => 'הצבעות',
+        'thanks'        => 'תודה',
+        'outOf'         => 'מתוך',
+        'weRecommend'   => 'אנו ממליצים',
     );
-?>
