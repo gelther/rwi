df9785b44019eaf5dcf2449849585aeeddbb0322
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 13:23:09 +0200

    Code consistency changes for languages/ka.php

--- languages/ka.php
+++ languages/ka.php
@@ -1,19 +1,18 @@
 <?php
-    $dir = "ltr";
-    $hor = "right";
+    $dir        = 'ltr';
+    $hor        = 'right';
     $dictionary = array(
-        "rateAwful" => "საშინელი",
-        "ratePoor" => "ცუდი",
-        "rateAverage" => "საშუალო",
-        "rateGood" => "კარგი",
-        "rateExcellent" => "საუკეთესო",
-        "rateThis" => "შეაფასეთ",
-        "like" => "მომწონს",
-        "dislike" => "არ მომწონს",
-        "vote" => "ხმის მიცემა",
-        "votes" => "ხმათა რაოდენობა",
-        "thanks" => "მადლობა",
-        "outOf" => "/",
-        "weRecommend" => "ჩვენ გირჩევთ",
+        'rateAwful'     => 'საშინელი',
+        'ratePoor'      => 'ცუდი',
+        'rateAverage'   => 'საშუალო',
+        'rateGood'      => 'კარგი',
+        'rateExcellent' => 'საუკეთესო',
+        'rateThis'      => 'შეაფასეთ',
+        'like'          => 'მომწონს',
+        'dislike'       => 'არ მომწონს',
+        'vote'          => 'ხმის მიცემა',
+        'votes'         => 'ხმათა რაოდენობა',
+        'thanks'        => 'მადლობა',
+        'outOf'         => '/',
+        'weRecommend'   => 'ჩვენ გირჩევთ',
     );
-?>
