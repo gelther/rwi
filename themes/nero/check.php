e068286fda308460eeeeba08507407c54463f4b3
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-17 13:59:42 +0200

    Code consistency changes for themes/nero/check.php

--- themes/nero/check.php
+++ themes/nero/check.php
@@ -1,11 +1,10 @@
 <?php
-    $theme_options = new stdClass();
-    $theme_options->type = "nero";
-    $theme_options->style = "check";
-        
+    $theme_options        = new stdClass();
+    $theme_options->type  = 'nero';
+    $theme_options->style = 'check';
+
     $theme = array(
-        "name" => "check",
-        "title" => "Checkboxes (by Trevor H.)",
-        "options" => $theme_options
+        'name'    => 'check',
+        'title'   => 'Checkboxes (by Trevor H.)',
+        'options' => $theme_options
     );
-?>
