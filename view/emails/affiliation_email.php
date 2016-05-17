670b38d722c98382576c61499211eab6d5aae8f1
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-17 14:02:10 +0200

    Code consistency changes for view/emails/affiliation_email.php

--- view/emails/affiliation_email.php
+++ view/emails/affiliation_email.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * The view content of the email to send to affiliate@rating-widget.com
  * called from send_affiliate_application method in rating-widget.php
  */
@@ -11,42 +11,42 @@ extract( $VARS );
 	<tbody>
 		<tr>
 			<td><strong>Admin Email:</strong></td>
-			<td><?php 
+			<td><?php
 echo  $aff_admin_email ;
 ?>
 </td>
 		</tr>
 		<tr>
 			<td><strong>User ID:</strong></td>
-			<td><?php 
+			<td><?php
 echo  $aff_user_id ;
 ?>
 </td>
 		</tr>
 		<tr>
 			<td><strong>Site ID:</strong></td>
-			<td><?php 
+			<td><?php
 echo  $aff_site_id ;
 ?>
 </td>
 		</tr>
 		<tr>
 			<td><strong>Site Address:</strong></td>
-			<td><?php 
+			<td><?php
 echo  $aff_site_address ;
 ?>
 </td>
 		</tr>
 		<tr>
 			<td><strong>Posts #:</strong></td>
-			<td><?php 
+			<td><?php
 echo  $aff_total_posts ;
 ?>
 </td>
 		</tr>
 		<tr>
 			<td><strong>Comments #:</strong></td>
-			<td><?php 
+			<td><?php
 echo  $aff_total_comments ;
 ?>
 </td>
