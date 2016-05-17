7671adde4417d3d69d907d19a5fd45b7786380ac
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-17 14:02:06 +0200

    Code consistency changes for view/comment_rating_mode_options.php

--- view/comment_rating_mode_options.php
+++ view/comment_rating_mode_options.php
@@ -11,58 +11,58 @@ $comment_ratings_mode = rw_settings()->comment_ratings_mode;
 <div id="rw_comment_rating_mode_settings" class="has-sidebar has-right-sidebar">
 	<div class="has-sidebar-content">
 		<div class="postbox rw-body">
-			<h3><?php 
+			<h3><?php
 _erw( 'comment-ratings-mode' );
 ?>
 </h3>
 
 			<div class="inside rw-ui-content-container rw-no-radius">
-				<div class="rw-ui-img-radio rw-ui-hor<?php 
+				<div class="rw-ui-img-radio rw-ui-hor<?php
 if ( 'false' === $comment_ratings_mode ) {
     echo  ' rw-selected' ;
 }
 ?>
 ">
 					<i class="rw-ui-sprite"></i> <input type="radio" name="rw_comment_review_mode"
-					                                    value="false" <?php 
+					                                    value="false" <?php
 checked( 'false', $comment_ratings_mode );
 ?>
 >
-					<span><?php 
+					<span><?php
 _erw( 'comment-ratings-mode_standard' );
 ?>
 </span>
 				</div>
 				<div
-					class="rw-ui-img-radio rw-ui-img-radio-review-mode rw-ui-hor<?php 
+					class="rw-ui-img-radio rw-ui-img-radio-review-mode rw-ui-hor<?php
 if ( 'true' === $comment_ratings_mode ) {
     echo  ' rw-selected' ;
 }
 ?>
 ">
 					<i class="rw-ui-sprite"></i> <input type="radio" name="rw_comment_review_mode"
-					                                    value="true" <?php 
+					                                    value="true" <?php
 checked( 'true', $comment_ratings_mode );
 ?>
 >
-					<span><?php 
+					<span><?php
 _erw( 'comment-ratings-mode_reviews' );
 ?>
 </span>
 				</div>
 				<div
-					class="rw-ui-img-radio rw-ui-img-radio-review-mode rw-ui-hor<?php 
+					class="rw-ui-img-radio rw-ui-img-radio-review-mode rw-ui-hor<?php
 if ( 'admin_ratings' === $comment_ratings_mode ) {
     echo  ' rw-selected' ;
 }
 ?>
 ">
 					<i class="rw-ui-sprite"></i> <input type="radio" name="rw_comment_review_mode"
-					                                    value="admin_ratings" <?php 
+					                                    value="admin_ratings" <?php
 checked( 'admin_ratings', $comment_ratings_mode );
 ?>
 >
-					<span><?php 
+					<span><?php
 _erw( 'comment-ratings-mode_author' );
 ?>
 </span>
