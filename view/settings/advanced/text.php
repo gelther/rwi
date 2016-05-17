d799c17698148982ce545d102270d48be88a8b5e
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-17 14:03:03 +0200

    Code consistency changes for view/settings/advanced/text.php

--- view/settings/advanced/text.php
+++ view/settings/advanced/text.php
@@ -1,45 +1,45 @@
 <table id="rw_text_settings" cellspacing="0" style="display: none;">
-<?php 
+<?php
 global  $LNG_EN ;
 $odd = true;
-$i = 0;
+$i   = 0;
 require_once dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/languages/en.php';
 $text = rw_options()->advanced->text;
 foreach ( $LNG_EN as $key => $en_val ) {
     ?>
-    <tr id="rw_text_<?php 
+    <tr id="rw_text_<?php
     echo  $key ;
     ?>
-" class="rw-<?php 
+" class="rw-<?php
     echo  ( $odd ? 'odd' : 'even' ) ;
     ?>
 ">
-        <td<?php 
+        <td<?php
     if ( $i == 0 ) {
         echo  ' class="rw-ui-def-width"' ;
     }
     ?>
 >
-            <span class="rw-ui-def"><?php 
+            <span class="rw-ui-def"><?php
     echo  $en_val ;
     ?>
 :</span>
         </td>
         <td>
-            <input onfocus="var e = this; setTimeout(function(){jQuery(e).select();}, 100);" onblur="RWM.Set.text('<?php 
+            <input onfocus="var e = this; setTimeout(function(){jQuery(e).select();}, 100);" onblur="RWM.Set.text('<?php
     echo  $key ;
     ?>
-');" type="text" id="rw_text_input_<?php 
+');" type="text" id="rw_text_input_<?php
     echo  $key ;
     ?>
-" value="<?php 
+" value="<?php
     echo  esc_attr( $text->{$key} ) ;
     ?>
 " />
         </td>
     </tr>
-<?php 
-    $odd = !$odd;
+<?php
+    $odd = ! $odd;
     $i++;
 }
 ?>
